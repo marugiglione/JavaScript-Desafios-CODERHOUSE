dc405f8f5946137e257776c3b9a3ca9dae27d1dc
marugiglione <maru.giglione@gmail.com>
2022-07-06 18:14:17 -0300

    09. Entrega Final Proyecto

---
+++ 09. Entrega Final Proyecto/js/carrito.js
@@ -0,0 +1,47 @@
+const carrito = JSON.parse(localStorage.getItem("carrito"));
+
+let tbody = document.querySelector("#tbody");
+
+function rellenarCarrito(arrayCarrito)
+{
+    for (let producto of arrayCarrito)
+    {
+        let row = document.createElement("tr");
+        row.innerHTML = `<td class="px-5 py-3">${producto.nombreProductoElegido}</td><td class="px-5 py-3">$${producto.precioProductoElegido}</td><td class="px-5 py-3">${producto.cantidadProductoElegido}</td><td class="px-5 py-3">$${producto.subtotal}</td><td class="px-5 py-3"><button class="btn btn-danger eliminarProducto">Eliminar</button></td>`
+        tbody.appendChild(row);
+    }
+}
+
+rellenarCarrito(carrito);
+
+let botonesEliminar = document.querySelectorAll(".eliminarProducto");
+
+botonesEliminar.forEach((elemento, idx) => {
+    elemento.addEventListener("click", () => alertEliminarProducto(idx));
+})
+
+function alertEliminarProducto (idx){
+    const productoAEliminar = carrito[idx]
+    Swal.fire ({
+        title: `Está seguro de eliminar el producto ${productoAEliminar.nombreProductoElegido}?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, estoy seguro',
+        cancelButtonText: 'No, no quiero',
+    }).then ((result) => {
+        if (result.isConfirmed) {
+            addEventListener("click", (e) => eliminarProducto(e, idx));
+            Swal.fire({
+                title: 'El producto ha sido borrado con éxito',
+                icon: 'success',
+            });
+        }
+    }
+    )
+}
+
+function eliminarProducto(e, idx){
+    const newCarrito = [...carrito]
+    newCarrito.splice(idx, 1);
+    localStorage.setItem("carrito", JSON.stringify(newCarrito));
+}
---
+++ 09. Entrega Final Proyecto/js/productos.js
@@ -0,0 +1,39 @@
+// Opciones de productos a elegir
+const productos = [
+    {
+        id: 1,
+        img: "./img/tortaoreo.jpg",
+        nombre: "Torta Bomba Oreo",
+        precio: 2000,
+    },
+    {
+        id: 2,
+        img: "./img/tortacheescake.jpg",
+        nombre: "Torta Cheescake",
+        precio: 2100,
+    },
+    {
+        id: 3,
+        img: "./img/tortakeylimepie.jpg",
+        nombre: "Torta Lime Pie",
+        precio: 1800,
+    },
+    {
+        id: 4,
+        img: "./img/budin.zanahoria.jpg",
+        nombre: "Budín de zanahoria",
+        precio: 1300,
+    },
+    {
+        id: 5,
+        img: "./img/tortabrownie.png",
+        nombre: "Torta Brownie",
+        precio: 2200,
+    },
+    {
+        id: 6,
+        img: "./img/tortamississipi.jpg",
+        nombre: "Torta Mississipi",
+        precio: 2200,
+    },
+]
